d6a961be0910a24326bbbcdac7845d0ef4616021
noreplay <noreplay>
2022-03-30 21:15:58 +0530

    create reminder fun - bug in showing time left fixed

--- reminder.py
+++ reminder.py
@@ -1,43 +1,58 @@
-from lib2to3.pytree import convert
-from math import remainder
 import sqlite3
 import re  
 from datetime import date, datetime,timedelta
+from time import time
 
-conn = sqlite3.connect("remiders.db")
-c = conn.cursor()
+
 
 def convert(t1):
     try: 
         h,m = t1.split(":")
         min = (int(h)*60)+int(m)
+        print(min,h)
         return min
     except:
         hour = timedelta( minutes=t1)
         return hour
 
-def create_reminder():
-    rtime = "17:43"#in 24 clock
-    reminder_name = "workout"
-    remarks = "do 100 pushups"
+def create_reminder(rtime,reminder_name,remarks):
+    #rtime = "17:43"#in 24 clock
+    #reminder_name = "workout"
+    #remarks = "do 100 pushups"
 
     if len(reminder_name) >0 and len(rtime)>0:
         time_now_tmp = datetime.now()
         #print(time_now_tmp)
         time_now= re.search(r" (.*:.*):",str(time_now_tmp))
-        h,m= rtime.split(":")
-        if time_now.group(1) <= rtime:
-            diff = convert(time_now.group(1)) - convert(rtime)
-            print(diff)
+        h1,m1= rtime.split(":")
+
+        y= time_now_tmp.year
+        m= time_now_tmp.month
+        d= time_now_tmp.day
+        t1 = datetime(y,m,d,int(h1),int(m1),0)
+        diff = t1 - datetime.now()
+        #print(diff)
+        try:
+            time_left = re.search(r", (.*):(.*):(.*)",str(diff))
+            h2,m2,s2 = time_left.group(1),time_left.group(2),time_left.group(3)
+        except:
+            time_left = re.search(r"(.*):(.*):(.*)",str(diff))
+            h2,m2,s2 = time_left.group(1),time_left.group(2),time_left.group(3)
+        
+        if alarm_bit:
+            print("")
+        
+        elif int(h2)>0:
+            print(f"{h2} hours and {m2} minutes left")
+        elif int(h2) == 0 and m>0:
+            print(f"{m2} minutes left")
+      
         else:
-            diff = (convert(time_now.group(1)) - convert(rtime)) + 1440
-            print(diff)
-        diff=convert(diff)
-        print(f"{diff} remaining")
+            print("less than 1 minute left")
 
         remind = {reminder_name:{rtime:remarks}}
         return remind,time_now.group(1)
-
+             
 def add_to_db(remind):
     c.execute("DROP TABLE IF EXISTS reminder")
     c.execute('''CREATE TABLE if not exists reminder ( reminder_name varchar(255) NOT NULL,time varchar(255) NOT NULL,remarks varchar(255) )''')
@@ -63,14 +78,17 @@ def alarm(time_now):
     for d in data:
         if time_now in d :
             print("_"*20+"ALARM"+"_"*20)
-            print(d)
+            print(f"time : {d[1]}\nreminder: {d[0]}\ngoal: {d[2]}")
             c.execute(f"DELETE FROM reminder where time = '{time_now}'")
-    show_reminder()
+            alarm_bit = True
     conn.commit()
+    return alarm_bit
+
+if __name__ == "__main__" :
+    conn = sqlite3.connect("remiders.db")
+    c = conn.cursor()
+    remind,time_now = create_reminder("21:10","workout","100 pushups")
+    add_to_db(remind)
+    #show_reminder()
+    alarm_bit = alarm(time_now)
     conn.close()
-
-
-remind,time_now = create_reminder()
-add_to_db(remind)
-show_reminder()
-alarm(time_now)
